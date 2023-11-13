5d3ddb0ecadf656139d5f2ca9ebff9ff241d0e82
Ángel Nicolás Di Falco <difalcoangelnicolas@gmail.com>
2023-11-13 10:20:20 -0300

    itemLisContainer Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { ItemListContainer } from './componets/ItemListContainer/ItemListContainer'
 import { NavBar } from './componets/NavBar/NavBar'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 export const App = () => {
   return (
     <>
     <h2>Polirrubros Pi-Va-So</h2>
     <img src='../public/images/logo.png' alt="logo" width={"200px"}/>
+    <BrowserRouter>
     <NavBar/>
-    <ItemListContainer menssage={"Bienvenido"}/>
+    <Routes>
+    <Route path="/" element={ <ItemListContainer menssage={"Bienvenido"}/> } />
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
