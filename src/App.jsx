3ca7c95de3702072bbfcf96376ee30da8e7dc490
Ángel Nicolás Di Falco <difalcoangelnicolas@gmail.com>
2023-11-13 09:49:59 -0300

    react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ export const App = () => {
   return (
     <>
     <h2>Polirrubros Pi-Va-So</h2>
-    <img src='../public/images/logo.png' alt="logo" width={"200px"} ></img>
+    <img src='../public/images/logo.png' alt="logo" width={"200px"}/>
     <NavBar/>
     <ItemListContainer menssage={"Bienvenido"}/>
     </>
